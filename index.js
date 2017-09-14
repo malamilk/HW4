098f70d50ff90ad0d0448af33b7522d4fcf3dd02
malamilk <malamilk.milk@gmail.com>
2017-09-14 09:55:12 +0700

    Sending Homework

---
+++ index.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+
+var app= express();
+app.set('num', 1);
+var num = app.get('num');
+
+
+console.log(num); //1
+
+app.use(function(req, res, next) {
+    console.log('Record timestamp');
+    next();
+});
+
+app.use(bodyparser.json());
+app.post('/calculator/rest/plus', function(req, res, next) {
+    var body = req.body;
+    var x = body.x;
+    var y = body.y;
+    var result = 0;
+    var obj = {
+        "x" : x,
+        "y" : y,
+        "result" : x + y
+    }
+    res.json(obj);
+});
+
+app.post('/calculator/rest/minus', function(req, res, next) {
+    var body = req.body;
+    var x = body.x;
+    var y = body.y;
+    var result = 0;
+    var obj = {
+        "x" : x,
+        "y" : y,
+        "result" : x - y
+    }
+    res.json(obj);
+});
+
+app.post('/calculator/rest/multiply', function(req, res, next) {
+    var body = req.body;
+    var x = body.x;
+    var y = body.y;
+    var result = 0;
+    var obj = {
+        "x" : x,
+        "y" : y,
+        "result" : x * y
+    }
+    res.json(obj);
+});
+app.get('/help', function(req, res, next) {
+    res.send('Help me');
+});
+
+
+app.listen(3000, function() {
+    console.log('Starting node on part 3000');
+});
